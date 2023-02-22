59edfdcce9f70211d7219dfc3b7e698302153d3a
Julian Lohuis <lohuisjulian@gmail.com>
2023-02-22 17:48:46 +0100

    Changed sort order of files and unhidden files

--- after/plugin/neo-tree.lua
+++ after/plugin/neo-tree.lua
@@ -2,13 +2,18 @@ vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
 
 require("neo-tree").setup({
     enable_git_status = true,
-    close_if_last_window = true,
+    sort_case_insensitive = true,
     window = {
         width = 40
     },
     filesystem = {
         hijack_netrw_behavior = "open_default",
-        use_libuv_file_watcher = true
+        use_libuv_file_watcher = true,
+        filtered_items = {
+            hide_dotfiles = false,
+            hide_gitignored = false,
+            hide_hidden = false
+        }
     }
 })
 
