cc58349d65bf9eb37e7886408e6238427f1931a0
Julian Lohuis <lohuisjulian@gmail.com>
2024-10-31 00:47:28 +0100

    Changes

--- init.lua
+++ init.lua
@@ -84,7 +84,7 @@ vim.keymap.set('v', 'K', ':m \'<-2<CR>gv=gv')
 vim.keymap.set('n', '<leader>y', '\"+y')
 vim.keymap.set('v', '<leader>y', '\"+y')
 vim.keymap.set('n', '<leader>Y', '\"+Y')
-vim.keymap.set('n', ',y', ':%y+<CR>')
+vim.keymap.set('n', '<M-y>', ':%y+<CR>')
 
 vim.keymap.set('i', '<C-c>', '<Nop>')
 
@@ -371,7 +371,7 @@ require('lazy').setup({
     'neovim/nvim-lspconfig',
     dependencies = {
       -- Automatically install LSPs and related tools to stdpath for neovim
-      { 'williamboman/mason.nvim', config = true },
+      { 'williamboman/mason.nvim', config = { PATH="append" } },
       'williamboman/mason-lspconfig.nvim',
       'WhoIsSethDaniel/mason-tool-installer.nvim',
 
@@ -520,9 +520,52 @@ require('lazy').setup({
       --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
+      local rust_analyzer = {
+        checkOnSave = {
+          command = "clippy",
+        },
+      }
+      -- if vim.fn.getcwd() == '/home/julo/Projects/rust' then
+      --   rust_analyzer = {
+      --     check = {
+      --       invocationLocation = "root",
+      --       invocationStrategy = "once",
+      --       overrideCommand = { "python3", "x.py", "check", "--json-output" },
+      --     },
+      --     linkedProjects = {
+      --       "Cargo.toml",
+      --       "library/Cargo.toml",
+      --       "src/tools/x/Cargo.toml",
+      --       "src/bootstrap/Cargo.toml",
+      --       "src/tools/rust-analyzer/Cargo.toml",
+      --       "compiler/rustc_codegen_cranelift/Cargo.toml",
+      --       "compiler/rustc_codegen_gcc/Cargo.toml",
+      --     },
+      --     procMacro = {
+      --       server = "/home/julo/Projects/rust/build/host/stage0/libexec/rust-analyzer-proc-macro-srv",
+      --       enable = true,
+      --     },
+      --     cargo = {
+      --       buildScripts = {
+      --         enable = true,
+      --         invocationLocation = "root",
+      --         invocationStrategy = "once",
+      --         overrideCommand = { "python3", "x.py", "check", "--json-output" },
+      --       },
+      --       sysrootSrc = "./compiler",
+      --       extraEnv = { RUSTC_BOOTSTRAP = 1 },
+      --     },
+      --     rustc = { source = "./Cargo.toml" },
+      --   }
+      -- end
       local servers = {
         clangd = {
-          cmd = { "clangd", "--header-insertion=never" },
+          cmd = { "clangd", "--header-insertion=never", "--background-index", "-j", "8" },
+        },
+        rust_analyzer = {
+          settings = {
+            ["rust-analyzer"] = rust_analyzer,
+          },
         },
         lua_ls = {
           -- cmd = {...},
